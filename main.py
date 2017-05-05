f3768fc008c9ff018bc40796129752ad137aaa85
shenjiawei <shenjiawei19@sina.com>
2017-05-05 10:20:37 +0800

    0.045 0.07 0.0547

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-from action.action_first import action_first
-from action.action_second import action_second
-from action.action_third import action_third
-from action.action_fourth import action_fourth
-from action.action_sixth import action_sixth
+import pandas as pd
 from action.action_predict_first import action_predict_first
-from action.action_predict_third import action_predict_third
-from train.train_first import train_first
 from train.vialid import vialid
-# a = action_first('JData_Action_201602.csv')
-# print (a)
-# print (action_second())
-# print (action_third('2016-02-02','2016-02-16'))
-# print (action_fourth())
-# print (action_sixth())
-# print (action_predict_first('JData_Action_201604.csv', '2016-04-06', '2016-04-10'))
-# print (action_predict_third())
-# print (train_first('JData_Action_201604.csv','2016-04-11','2016-04-15'))
-vialid('predict_result-2.csv', 'train_result.csv')
+
+print (action_predict_first('JData_Action_201604.csv', '2016-04-06', '2016-04-10'))
+
+df = pd.read_csv('predict1.csv')
+df2 = df.groupby(by=['user_id','sku_id'])['1','2','3','4','5','6'].sum().reset_index()
+df3 =df2.rename(index=str, columns={"1": "act1", "2": "act2",'3':'act3','4':'act4','5':'act5','6':'act6'})
+df3['rate'] = df3.apply(
+    lambda x: (x.act1) * 1.2 + (x.act2) * 33 + (x.act3) * (-20) + (x.act4) * (-1000) + (x.act5) * 1 + (x.act6) * 2.1,
+    axis=1)
+df4 = df3.sort_values(by='rate', ascending=0).head(1000)
+df5 = df4.loc[:, ['user_id', 'sku_id']]
+df5 = df5.drop_duplicates(['user_id'])
+result = vialid(df5, 'train_result.csv')
+print (result)
+# df5.to_csv('20170505.csv',index=False)
